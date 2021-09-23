97f91b3595d00a5b2055147920f61dd01fc448d9
Joys101 <joys.showvick.biswas@g.bracu.ac.bd>
2021-09-23 23:33:20 +0600

    Daily-Scheduling-App

---
+++ app/src/test/java/com/frsarker/todotask/MainActivityTest.java
@@ -0,0 +1,18 @@
+package com.frsarker.todotask;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+    @Test
+    public void testopenAddModifyTask()
+    {
+    };
+}
